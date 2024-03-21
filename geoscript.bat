c1df4e92ed25162d25fa4ac277ab9ac6c3a5e356
DaylumGMD <140441146+DaylumGD@users.noreply.github.com>
2024-03-21 17:10:15 +0000

    Fixed command line interface (ver 1.0)

--- geoscript.bat
+++ geoscript.bat
@@ -1,13 +1,15 @@
 @echo off
 
-set command = %1
-set arg1 = %2
-set arg2 = %3
+set "command=%1"
+set "arg1=%2"
+set "arg2=%3"
 
-if %command%==build cmd /c python -c "import GeoScript; GeoScript.build(%arg1%, %arg2%)"
-
-else if %command%==settings cmd /c python -c "import GeoScript; GeoScript.settings()"
-
-else if %command%==documentation cmd /c python -c "import GeoScript; GeoScript.documentation()"
-
-else cmd /c python -c "import GeoScript; GeoScript.main()"
+if "%command%"=="build" (
+    cmd /c python -c "import GeoScript; GeoScript.build('%arg1%', '%arg2%')"
+) else if "%command%"=="settings" (
+    cmd /c python -c "import GeoScript; GeoScript.settings()"
+) else if "%command%"=="documentation" (
+    cmd /c python -c "import GeoScript; GeoScript.documentation()"
+) else (
+    cmd /c python -c "import GeoScript; GeoScript.main()"
+)
